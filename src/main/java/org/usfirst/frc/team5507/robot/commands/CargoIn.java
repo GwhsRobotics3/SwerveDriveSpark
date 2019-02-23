bf3dd4188048d2d95674f265f2f62ddeb8359637
GwhsRobotics3 <gwhsrobotics129@gmail.com>
2019-02-23 14:09:41 -0800

    deleted a bunch of lines !!!!!!!!!!!!!!!!!

--- src/main/java/org/usfirst/frc/team5507/robot/commands/CargoIn.java
+++ src/main/java/org/usfirst/frc/team5507/robot/commands/CargoIn.java
@@ -13,35 +13,27 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class CargoIn extends Command {
   public CargoIn() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
     requires(Robot.m_cargo);
   }
 
-  // Called just before this Command runs the first time
   @Override
   protected void initialize() {
   }
 
-  // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
       Robot.m_cargo.pullCargo();
   }
 
-  // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
     return false;
   }
 
-  // Called once after isFinished returns true
   @Override
   protected void end() {
   }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
   @Override
   protected void interrupted() {
   }
--- src/main/java/org/usfirst/frc/team5507/robot/commands/HatchIn.java
+++ src/main/java/org/usfirst/frc/team5507/robot/commands/HatchIn.java
@@ -16,36 +16,28 @@ public class HatchIn extends Command {
   
   public Timer timer = new Timer();
   public HatchIn() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
     requires(Robot.m_HatchDelivery);
   }
 
-  // Called just before this Command runs the first time
   @Override
   protected void initialize() {
   }
 
-  // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
     Robot.m_HatchDelivery.retractHatch();
   }
 
-  // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
     return false;
   }
 
-  // Called once after isFinished returns true
   @Override
   protected void end() {
     
   }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
   @Override
   protected void interrupted() {
   }
