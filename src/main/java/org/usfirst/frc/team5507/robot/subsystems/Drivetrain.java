a901ca1530183cf0711c9edbdb2232eed161c390
GwhsRobotics3 <gwhsrobotics129@gmail.com>
2019-03-21 10:34:41 -0700

    3/21/19 SR Competition Code

--- src/main/java/org/usfirst/frc/team5507/robot/subsystems/Drivetrain.java
+++ src/main/java/org/usfirst/frc/team5507/robot/subsystems/Drivetrain.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team5507.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public abstract class Drivetrain extends Subsystem {
-	private double speedMultiplier = .7;
+	private double speedMultiplier = .825;
 
 	protected abstract void initDefaultCommand();
 
